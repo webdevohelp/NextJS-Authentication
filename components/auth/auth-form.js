90584103a76e83cbde1591cfdde24829b3362716
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2022-03-07 18:11:30 +0530

    started working on signin api
    started working on signin api

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,38 +1,101 @@
-import { useState } from 'react';
+// @ts-nocheck
+import { useState, useRef } from 'react';
 import classes from './auth-form.module.css';
 
+import { signIn, signin } from 'next-auth/client';
+
+async function createUser(email, password) {
+    const response = await fetch('/api/auth/signup', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong');
+    }
+
+    return data;
+}
+
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
 
-  function switchAuthModeHandler() {
-    setIsLogin((prevState) => !prevState);
-  }
+    const [isLogin, setIsLogin] = useState(true);
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+    function switchAuthModeHandler() {
+        setIsLogin((prevState) => !prevState);
+    }
+
+    async function submitHandler(event) {
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        // OPTIONAL : add custom validations here
+
+        if (isLogin) {
+            // log user in
+            const result = await signIn('credentials', {
+                redirect: false,
+                email: enteredEmail,
+                password: enteredPassword,
+            });
+            console.log(result);
+        } else {
+            // create user
+            try {
+                const result = await createUser(enteredEmail, enteredPassword);
+                console.log(result);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+    return (
+        <section className={classes.auth}>
+            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor="email">Your Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        ref={emailInputRef}
+                        required
+                    />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="password">Your Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        ref={passwordInputRef}
+                        required
+                    />
+                </div>
+                <div className={classes.actions}>
+                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                    <button
+                        type="button"
+                        className={classes.toggle}
+                        onClick={switchAuthModeHandler}
+                    >
+                        {isLogin
+                            ? 'Create new account'
+                            : 'Login with existing account'}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
 }
 
 export default AuthForm;
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+// @ts-nocheck
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+export default NextAuth({
+    session: {
+        jwt: true,
+    },
+    providers: [
+        Providers.Credentials({
+            async authorize(credentials) {
+                const client = await connectToDatabase();
+                const usersCollection = client.db().collection('users');
+
+                const user = await usersCollection.findOne({
+                    email: credentials.email,
+                });
+
+                if (!user) {
+                    throw new Error('No user found');
+                }
+                const isValid = await verifyPassword(
+                    credentials.password,
+                    user.password
+                );
+                if (!isValid) {
+                    throw new Error("Couldn't log in!");
+                    client.close();
+                }
+                client.close();
+                return { email: user.email };
+            },
+        }),
+    ],
+});
